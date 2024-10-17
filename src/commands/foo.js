66af7d3909319aa7aa0d22c1747ed8f317371b81
Chris Troutner <chris.troutner@gmail.com>
2024-10-17 06:43:12 -0800

    fix(wallet-list): Added wallet-list command with 100% unit test coverage

--- src/commands/foo.js
+++
@@ -1,9 +0,0 @@
-/*
-  The 'foo' command
-*/
-
-async function start (argv) {
-  console.log('argv: ', argv)
-  console.log('The foo command was executed.')
-}
-start()
---
+++ test/unit/wallet-list.js
@@ -0,0 +1,88 @@
+/*
+  Unit tests for the wallet-list command.
+*/
+
+// Global npm libraries
+import { assert } from 'chai'
+import sinon from 'sinon'
+import { promises as fs } from 'fs'
+
+// Local libraries
+import WalletCreate from '../../src/commands/wallet-create.js'
+import WalletList from '../../src/commands/wallet-list.js'
+
+const __dirname = import.meta.dirname
+const filename = `${__dirname.toString()}/../../.wallets/test123.json`
+
+describe('#wallet-list', () => {
+  let sandbox
+  let uut
+
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox()
+
+    uut = new WalletList()
+  })
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  describe('#parseWallets', () => {
+    it('should correctly parse wallet data', async () => {
+      // Create a mainnet wallet.
+      const createWallet = new WalletCreate()
+      await createWallet.createWallet(filename)
+
+      const data = await uut.parseWallets()
+      // console.log('data: ', data)
+
+      // Find the wallet that was just created.
+      const testWallet = data.find(wallet => wallet[0].indexOf('test123') > -1)
+      // console.log('testWallet: ', testWallet)
+
+      assert.include(testWallet[0], 'test123')
+
+      // Clean up
+      await fs.rm(filename)
+    })
+
+    it('should return empty array on missing wallets data', async () => {
+      // Force shelljs.ls to return an empty array.
+      sandbox.stub(uut.shelljs, 'ls').returns([])
+
+      let data
+
+      try {
+        data = await uut.parseWallets()
+      } catch (error) {
+        assert.equal(data, [], 'Empty array')
+        assert.equal(error, 'No wallets found.', 'Proper error message')
+      }
+    })
+  })
+
+  describe('#run', () => {
+    it('should display wallets table', async () => {
+      const createWallet = new WalletCreate()
+      await createWallet.createWallet(filename, 'test wallet')
+
+      Promise.resolve(uut.run()).then(function (table) {
+        assert.include(table, 'Name')
+        assert.include(table, 'Balance (BCH)')
+      })
+
+      // Clean up
+      await fs.rm(filename)
+    })
+
+    it('should return 0 on error', async () => {
+      // Force an error
+      sandbox.stub(uut, 'parseWallets').throws(new Error('test error'))
+
+      const result = await uut.run()
+
+      assert.equal(result, 0)
+    })
+  })
+})
